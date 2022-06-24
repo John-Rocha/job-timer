cd8638f698885c254e11ab89a619978e32f21d00
Johnathan Rocha <johnathanrocha@gmail.com>
2022-06-24 17:52:23 -0300

    chore: add interceptor of firebase auth

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,9 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/core/consts.dart';
 import 'package:job_timer/app/core/ui/app_config_ui.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user == null) {
+        Modular.to.navigate('/login');
+      } else {
+        Modular.to.navigate('/home');
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
